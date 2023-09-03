086904805dbddfa401917aa97abb65a8c06cc5f5
aivarasimkus9@gmail.com <aivarasimkus9@gmail.com>
2023-09-03 14:17:43 +0300

    naujausias

--- baigiamasis/products/views.py
+++ baigiamasis/products/views.py
@@ -26,7 +26,7 @@ def scrape_products(url):
 
 
 def scrape_recipes(product_name):
-    url = 'https://www.receptai.lt/paieska?file'
+    url = 'https://www.receptai.lt/paieska?'
     params = {'q': product_name}
 
 
@@ -49,13 +49,24 @@ def scrape_recipes(product_name):
 
 
 def index(request):
-    mesos_url = "https://www.rimi.lt/e-parduotuve/lt/akcijos?page=1&pageSize=20&query=%3Arelevance%3AassortmentStatus%3AinAssortment%3AinStockFlag%3Atrue%3AfixedPrice%3Afalse%3AfirstLevelCategory%3AM%25C4%2597sa%252C%2B%25C5%25BEuvys%2Bir%2Bkulinarija"
+    chicken_url = "https://www.rimi.lt/e-parduotuve/lt/akcijos?page=1&pageSize=100&query=%3Arelevance%3AassortmentStatus%3AinAssortment%3AinStockFlag%3Atrue%3AfixedPrice%3Afalse%3AfirstLevelCategory%3AM%25C4%2597sa%252C%2B%25C5%25BEuvys%2Bir%2Bkulinarija%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3Acategory%3A%25C5%25A0vie%25C5%25BEia%2Bpauk%25C5%25A1tiena"
+    fish_url = "https://www.rimi.lt/e-parduotuve/lt/akcijos?page=1&pageSize=100&query=%3Arelevance%3AassortmentStatus%3AinAssortment%3AinStockFlag%3Atrue%3AfixedPrice%3Afalse%3AfirstLevelCategory%3AM%25C4%2597sa%252C%2B%25C5%25BEuvys%2Bir%2Bkulinarija%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3Acategory%3A%25C5%25A0vie%25C5%25BEios%2B%25C5%25BEuvys%2Bir%2Bj%25C5%25ABr%25C5%25B3%2Bg%25C4%2597ryb%25C4%2597s"
+    pork_and_beef_url = "https://www.rimi.lt/e-parduotuve/lt/akcijos?page=1&pageSize=100&query=%3Arelevance%3AassortmentStatus%3AinAssortment%3AinStockFlag%3Atrue%3AfixedPrice%3Afalse%3AfirstLevelCategory%3AM%25C4%2597sa%252C%2B%25C5%25BEuvys%2Bir%2Bkulinarija%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3Acategory%3A%25C5%25A0vie%25C5%25BEia%2Bm%25C4%2597sa"
+    processed_meat_url = "https://www.rimi.lt/e-parduotuve/lt/akcijos?page=1&pageSize=100&query=%3Arelevance%3AassortmentStatus%3AinAssortment%3AinStockFlag%3Atrue%3AfixedPrice%3Afalse%3AfirstLevelCategory%3AM%25C4%2597sa%252C%2B%25C5%25BEuvys%2Bir%2Bkulinarija%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3Acategory%3AApdorotos%2Bm%25C4%2597sos%2Bir%2Bpauk%25C5%25A1tienos%2Bgaminiai%2B"
+    processed_fish_url = "https://www.rimi.lt/e-parduotuve/lt/akcijos?page=1&pageSize=100&query=%3Arelevance%3AassortmentStatus%3AinAssortment%3AinStockFlag%3Atrue%3AfixedPrice%3Afalse%3AfirstLevelCategory%3AM%25C4%2597sa%252C%2B%25C5%25BEuvys%2Bir%2Bkulinarija%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3AfixedPrice%3Afalse%3Acategory%3AApdorotos%2B%25C5%25BEuvies%2Bgaminiai"
     products = []
     recipes = []
 
-    if request.method == 'GET' and 'product_type' in request.GET and request.GET.get('product_type') == 'mesa':
-        products = scrape_products(mesos_url)
-
+    if request.method == 'GET' and 'product_type' in request.GET and request.GET.get('product_type') == 'chicken':
+        products = scrape_products(chicken_url)
+    if request.method == 'GET' and 'product_type' in request.GET and request.GET.get('product_type') == 'fish':
+        products = scrape_products(fish_url)
+    if request.method == 'GET' and 'product_type' in request.GET and request.GET.get('product_type') == 'pork_and_beef':
+        products = scrape_products(pork_and_beef_url)
+    if request.method == 'GET' and 'product_type' in request.GET and request.GET.get('product_type') == 'processed_meat':
+        products = scrape_products(processed_meat_url)
+    if request.method == 'GET' and 'product_type' in request.GET and request.GET.get('product_type') == 'processed_fish':
+        products = scrape_products(processed_fish_url)
 
 
     if request.method == 'GET' and 'product_name' in request.GET:
